a3dc58e73d65b42e7b20b088ab54030e7e2bea35
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-06-28 19:18:21 +0530

    Create bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,15 @@
+class bitwise
+{
+    public static void main(String agrs[])
+{
+    int a=2;
+    int b=6;
+    System.out.println(a|b);
+    System.out.println(a&b);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    System.out.println(a^b);
+    System.out.println(~a);
+    System.out.println(~b);
+    System.out.println(a<<b);
+    System.out.println(a>>b);
+}
+}
