befb3b6e56debe2e3a82924e2aaf1226fce57a62
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-06-28 19:19:43 +0530

    Create task.c

--- task.c
+++ task.c
@@ -1,7 +1,7 @@
 //code for basic mathematical task that adds two variables a and b and multiplies resultant c again with a and them with b and prints resultant d.
 class task
 {
-public static void main(String agrs[])
+public static void main(String []args)
 {
     int a=10,b=5;
     int c=a+b;
