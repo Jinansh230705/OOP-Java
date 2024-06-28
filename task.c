419acef403e956235272bfe5f473a781495cbf6d
Jinansh <149217594+Jinansh230705@users.noreply.github.com>
2024-06-28 19:16:12 +0530

    Create task.c

---
+++ task.c
@@ -0,0 +1,12 @@
+//code for basic mathematical task that adds two variables a and b and multiplies resultant c again with a and them with b and prints resultant d.
+class task
+{
+public static void main(String agrs[])
+{
+    int a=10,b=5;
+    int c=a+b;
+    int d=c*a*b;
+
+System.out.println(d);
+}
+}
